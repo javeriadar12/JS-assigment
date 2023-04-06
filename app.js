9886be872b2ccee6da1e396382866de79afbdffc
javeriadarwaish <javeriadarwaish@gmail.com>
2023-04-05 22:12:24 -0700

    first assignment of js.

---
+++ app.js
@@ -0,0 +1,25 @@
+// -----------question 1---------------------
+// write a script to greet your website visitor using JS alert box.
+
+// alert("Welcome to the website! ");
+
+// -----------question 2-----------------------
+// write a script to disply following message on your web page:
+
+// alert("Error!please enter a valid password.");
+
+// -----------question 3-----------------------
+// write a script to display following message on your web page:
+
+// alert("welcome to JS land....\nHappy coding!");
+
+// -------------question 4-------------------
+// write a script to display following messages in sequence:
+
+// alert("welcome to JS land.....");
+// alert("Happy coding!\nPrevent this page from creating additional dialogs.");
+
+// ----------question 5--------------------
+// Generate the following message through browser's developer console
+
+alert("Hello.... I can run JS through my web browser's console");
